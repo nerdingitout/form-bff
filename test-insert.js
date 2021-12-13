34aea226a8b4a5364fab2665adad768d97898b24
Masa Abushamleh <36239840+nerdingitout@users.noreply.github.com>
2021-12-13 15:44:29 +0400

    Update test-insert.js

--- test-insert.js
+++ test-insert.js
@@ -3,11 +3,11 @@ const { Pool, Client } = require("pg");
 
 const pool = new Pool({
 /* do not put password or any sensitive info here, done only for demo */
-    host: env.DB_HOST,
-    port: env.DB_PORT,
-    user: env.DB_USER,
-    password: env.DB_PASSWORD,
-    database: env.DB_NAME,
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
     ssl: {
         require: true,
         rejectUnauthorized: false
