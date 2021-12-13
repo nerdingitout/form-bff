7cb4d690e20481cd811cbbcbd3fe6673e93e2490
Masa Abushamleh <36239840+nerdingitout@users.noreply.github.com>
2021-12-13 15:44:59 +0400

    Update config.js

--- config.js
+++ config.js
@@ -2,11 +2,11 @@
 
 const config = {
   db: { /* do not put password or any sensitive info here, done only for demo */
-    host: env.DB_HOST,
-    port: env.DB_PORT,
-    user: env.DB_USER,
-    password: env.DB_PASSWORD,
-    database: env.DB_NAME,
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
     ssl: {
         require: true,
         rejectUnauthorized: false
