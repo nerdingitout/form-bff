9cdd4d48d121ac6af69863fb8e332a3d46b51489
Masa Abushamleh <36239840+nerdingitout@users.noreply.github.com>
2021-12-13 14:44:48 +0400

    Update config.js

--- config.js
+++ config.js
@@ -1,23 +1,18 @@
-//require('dotenv').config();
-DB_HOST = os.environ.get('DB_HOST');
-DB_PORT = os.environ.get('DB_PORT');
-DB_USER = os.environ.get('DB_USER');
-DB_PASSWORD = os.environ.get('DB_PASSWORD');
-DB_NAME = os.environ.get('DB_NAME');
+require('dotenv').config();
 
 const config = {
   db: { /* do not put password or any sensitive info here, done only for demo */
-    host: DB_HOST,
-    port: DB_PORT,
-    user: DB_USER,
-    password: DB_PASSWORD,
-    database:DB_NAME,
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
     ssl: {
         require: true,
         rejectUnauthorized: false
     }
   },
-  listPerPage: 10,
+  listPerPage: process.env.LIST_PER_PAGE || 10,
 };
 
 module.exports = config;
