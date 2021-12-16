7b47b93e2cb8843bb3d3c10a67d47a7dfc808327
Masa Hilal H Abushamleh <Masa.Abushamleh@ibm.com>
2021-12-16 17:33:49 +0400

    kdealdkw

--- services/addBook.js
+++ services/addBook.js
@@ -5,13 +5,13 @@ const config = require('../config');
 
 async function insertBook() {
   // generating random id for book, we should check if the id exists in the table, if it doesnt exist we can add it
-  let book_id = Math.floor(Math.random() * 3000);
-  console.log(book_id);
+  //let book_id = Math.floor(Math.random() * 3000);
+  //console.log(book_id);
 
   //getting values from frontend, for now they are static
   let book_name='lkenfknfkns book';
   let book_author='jnfkjdncksn book author';
-
+  let book_id= 0;
   db.query(
     `insert into books values(${book_id},'${book_name}','${book_author}');`,
     (err, res) => {
