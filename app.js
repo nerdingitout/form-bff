48f7f94721dd056d13beb60cf26887d0db32bdbe
Masa Hilal H Abushamleh <Masa.Abushamleh@ibm.com>
2021-12-16 18:41:59 +0400

    dfjkhsjkvhd

--- app.js
+++ app.js
@@ -2,12 +2,14 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var cors = require('cors')
 
 var indexRouter = require('./routes/index');
 var quotesRouter = require('./routes/quotes');
 var booksRouter = require('./routes/books');
 var addBookRouter=require('./routes/addBook');
 var app = express();
+app.use(cors())
 
 app.use(logger('dev'));
 app.use(express.json());
